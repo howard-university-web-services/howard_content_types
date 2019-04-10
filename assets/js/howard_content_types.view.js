3437893795de60c7f14ff1d607e22816e57a1f42
Dan Rogers <dan.rogers@idfive.com>
2019-04-10 11:25:54 -0600

    Initial move from acquia hud8 repo.

---
+++ assets/js/howard_content_types.view.js
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * Defines the behavior of the custom Howard media entity browser view.
+ * Extends defailt media entity browser for custom view
+ */
+
+(function ($) {
+
+    "use strict";
+  
+    /**
+     * Attaches the behavior of the media entity browser view.
+     */
+    Drupal.behaviors.mediaEntityBrowserView = {
+      attach: function (context, settings) {
+        $('.views-row', context).once().click(function () {
+          var $row = $(this);
+          var $input = $row.find('.views-field-entity-browser-select input');
+          $input.prop('checked', !$input.prop('checked'));
+          $row[$input.prop('checked') ? 'addClass' : 'removeClass']('checked');
+        });
+      }
+    };
+  
+  }(jQuery, Drupal));
+  
